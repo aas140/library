7e23be8b1d7ee73923d273a8ea8d12445f5a0bb6
aastha <280506420+aas140@users.noreply.github.com>
2026-06-24 19:16:53 +0530

    Add a pop-up form to take book info through user input
    Added a dialog containing a form which asks for book title, author, number of pages, read status from the user

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const lib = [];
 
 const list = document.querySelector(".list");
 
+const addbtn = document.querySelector(".addBtn");
+
+const savebtn = document.querySelector(".save-book");
+
+const mydialog = document.querySelector("dialog");
+
+const dataform = document.querySelector("form");
+
 function Book(title, author, pages, read){
     this.id = crypto.randomUUID();
     this.title = title;
@@ -24,3 +32,25 @@ function display(){
 
     })
 }
+
+addbtn.addEventListener('click', () =>{
+    mydialog.showModal();
+})
+
+dataform.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").checked;
+
+    addBookToLib(title, author, pages, read);
+
+    console.log(lib);
+    
+    dataform.reset();
+    mydialog.close();
+})
+
+
